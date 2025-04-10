a5cd945265b95ed778024194a81aa2d3bed6b027
Atharva Jakhetiya <jakhetiyaatharva@gmail.com>
2025-04-10 20:09:52 +0530

    fixed runner 2.0

--- Runner2.0.py
+++ Runner2.0.py
@@ -21,6 +21,8 @@ threshold = 0.5
 
 cap = cv2.VideoCapture(0)
 
+sequence = []
+
 # Set mediapipe model 
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     while cap.isOpened():
